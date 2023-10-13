406d2ba621b0a1540e63633397b45ab3d8836705
Jun Wei <53926665+0xjunwei@users.noreply.github.com>
2023-10-13 18:39:01 +0800

    v0.2 updated hardhat config
    Updated hardhat config to taiko network to get ready for deployment of proofer later on

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,39 @@ require("@nomicfoundation/hardhat-toolbox");
 require("hardhat-deploy");
 
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    taiko: {
+      url: "https://rpc.jolnir.taiko.xyz",
+      accounts: [PRIVATE_KEY],
+      gasPrice: "auto",
+      chainId: 167005,
+    },
+  },
+  solidity: "0.8.9",
+  etherscan: {
+    apiKey: {
+      taiko: "42069",
+    },
+    customChains: [
+      {
+        network: "taiko",
+        chainId: 167007,
+        urls: {
+          apiURL: "https://blockscoutapi.jolnir.taiko.xyz/api",
+          browserURL: "https://explorer.jolnir.taiko.xyz",
+        },
+      },
+    ],
+  },
+  gasReporter: {
+    outputFile: "gas-report.txt",
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+    noColors: true,
+  },
 };
